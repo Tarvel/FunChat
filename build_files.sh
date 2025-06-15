c720b74f068dd557dff30db01db3293ffa8ee0a1
tarvel <taiyebabatundejames@gmail.com>
2025-06-16 00:31:39 +0100

    update

--- build_files.sh
+++ build_files.sh
@@ -2,10 +2,10 @@
 
 # Build the project
 echo "Building the project..."
-python3.11 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Collect Static..."
-python3.11 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 
 mkdir -p staticfiles_build
 cp -r staticfiles/* staticfiles_build/
