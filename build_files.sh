0e17121feac530b9dbbbac096073fc499d993085
tarvel <taiyebabatundejames@gmail.com>
2025-06-16 06:47:49 +0100

    update

--- build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build the project
-echo "Building the project..."
-python -m pip install -r requirements.txt
-
-echo "Collect Static..."
-python manage.py collectstatic --noinput --clear
--- funchat/settings.py
+++ funchat/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = False
 
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = ["*"]
 
 
 INSTALLED_APPS = [
